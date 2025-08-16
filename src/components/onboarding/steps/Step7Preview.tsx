30afbc4aa3131509c4becd9ffdb6eb1d56a170cc
KarelGustin <karelgustin@icloud.com>
2025-08-16 20:31:06 +0200

    open tabblad

--- src/components/onboarding/steps/Step7Preview.tsx
+++ src/components/onboarding/steps/Step7Preview.tsx
@@ -277,7 +277,7 @@ export const Step7Preview = ({
               </Button>
               <Button
                 variant="outline"
-                onClick={() => window.open(`${window.location.origin}/${handle}`, '_blank')}
+                onClick={() => window.open(`https://tapbookr.com/${handle}`, '_blank')}
                 disabled={!isLivePreviewActive}
               >
                 <ExternalLink className="w-4 h-4 mr-2" />
